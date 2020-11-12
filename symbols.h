3338e0cee3493f694729bc879790b92b7cde3372
aliu889 <32781686+aliu889@users.noreply.github.com>
2020-11-12 07:00:50 -0500

    Update symbols.h

--- symbols.h
+++ symbols.h
@@ -9,19 +9,14 @@ struct Symbols {
 };
 
 struct Symbols *new_Symbols();
-
 struct Symbols *addSong(struct Symbols *songlib, struct song_node *newsong);
-
-void print_Letter_Symbols(struct Symbols *lib, char c);
-
 void print_Symbols(struct Symbols *lib);
-
-void clear_lib(struct Symbols *lib);
-
+struct song_node *search(struct Symbols *lib, char *name, char *artist); 
+struct song_node *search_artist(struct Symbols *lib, char *artist);
+void print_Letter_Symbols(struct Symbols *lib, char c);
 void print_Artist_Symbols(struct Symbols *lib, char *a);
-
-struct Symbols *remove_song(struct Symbols *lib, struct song_node *song);
-
-struct Symbols *shuffle(struct Symbols *lib);
+struct Symbols *remove_song(struct Symbols *lib, char *name, char *artist);
+void clear_lib(struct Symbols *lib);
+void shuffle(struct Symbols *lib);
 
 #endif
