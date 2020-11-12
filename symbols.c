e505e2a15e1dc18b92cfb3a66b859ab37430f7cf
aliu889 <32781686+aliu889@users.noreply.github.com>
2020-11-12 07:00:32 -0500

    Update symbols.c

--- symbols.c
+++ symbols.c
@@ -9,58 +9,86 @@
 #include "songs.h"
 
 // Creates a new playlist.
-struct Symbols *new_Symbols() {
+struct Symbols *new_Symbols()
+{
   struct Symbols *newsymbols = malloc(sizeof(struct Symbols));
   int i;
-  for (i=0; i<27; i++) {
-    (newsymbols->library)[i]=NULL;
+  for (i = 0; i < 27; i++)
+  {
+    (newsymbols->library)[i] = NULL;
   }
   return newsymbols;
 }
 
 // Adds a song to the playlist according to the first letter of the artist's name.
 // This adds the song to the front of its respective alphabet list.
-// before was struct song_node
-struct Symbols *addSong(struct Symbols *songlib, struct song_node *newsong) {
-  int x = toupper((newsong->artist)[0]-65);
-  if ((songlib->library)[x]==NULL) (songlib->library)[x] = newsong;
-  else insert_song_sorted((songlib->library)[x], newsong);
-  // before was (songlib->library)[x]
+struct Symbols *addSong(struct Symbols *songlib, struct song_node *newsong)
+{
+  int x = toupper((newsong->artist)[0] - 65);
+  if ((songlib->library)[x] == NULL)
+    //insert_song_sorted((songlib->library)[x], newsong->name, newsong->artist);
+    (songlib->library)[x] = newsong;
+  else
+    insert_song_sorted((songlib->library)[x], newsong->name, newsong->artist);
+    //printlist_Song(songlib->library[x]);
   return songlib;
 }
 
 // Prints out the entire playlist alphabetically.
-void print_Symbols(struct Symbols *lib) {
+void print_Symbols(struct Symbols *lib)
+{
   int i;
-  for (i=0; i<27; i++) {
-    if ((lib->library)[i]!=NULL) {
-      printf("%c:\n", 'A'+i);
+  for (i = 0; i < 27; i++)
+  {
+    if ((lib->library)[i] != NULL)
+    {
+      printf("%c:\n", 'A' + i);
       printlist_Song((lib->library)[i]);
       printf("\n");
     }
   }
 }
 
+// Search for a song given song and artist name (as a pointer)
+struct song_node *search (struct Symbols *lib, char *name, char *artist) {
+  int x = toupper(artist[0] - 65);
+  return find_song((lib->library)[x], name, artist);
+}
+
+struct song_node *search_artist (struct Symbols *lib, char *artist) {
+  int x = toupper(artist[0] - 65);
+  return find_first_artist((lib->library)[x], artist);
+}
+
 // Prints out the songs in the playlist under a specific letter.
-void print_Letter_Symbols(struct Symbols *lib, char c) {
-  int x = toupper(c)-65;
-  if ((lib->library)[x]!=NULL) {
-    printf("%c:\n", 'A'+x);
+void print_Letter_Symbols(struct Symbols *lib, char c)
+{
+  int x = toupper(c) - 65;
+  if ((lib->library)[x] != NULL)
+  {
+    printf("%c:\n", 'A' + x);
     printlist_Song((lib->library)[x]);
     printf("\n");
   }
-  else {
-    if (x!=26) printf("%c:\nEmpty.\n\n", 'A'+x);
-    else printf("Other:\nEmpty.\n\n");
+  else
+  {
+    if (x != 26)
+      printf("%c:\nEmpty.\n\n", 'A' + x);
+    else
+      printf("Other:\nEmpty.\n\n");
   }
 }
 
-// print out all the songs of a certain artist 
-void print_Artist_Symbols(struct Symbols *lib, char *a) {
-  int x = toupper(a[0])-65;
-  if ((lib->library)[x] != NULL) {
-    while ((lib->library)[x]) {
-      if (strcmp( (lib->library)[x]->artist, a ) == 0) {
+// print out all the songs of a certain artist
+void print_Artist_Symbols(struct Symbols *lib, char *a)
+{
+  int x = toupper(a[0]) - 65;
+  if ((lib->library)[x] != NULL)
+  {
+    while ((lib->library)[x])
+    {
+      if (strcmp((lib->library)[x]->artist, a) == 0)
+      {
         printf("Artist: %s\tSong: %s\n", (lib->library)[x]->artist, (lib->library)[x]->name);
       }
       (lib->library)[x] = (lib->library)[x]->next;
@@ -68,36 +96,42 @@ void print_Artist_Symbols(struct Symbols *lib, char *a) {
   }
 }
 
-// clear the library 
-void clear_lib(struct Symbols *lib) {
-  int i;
-  while (i < 27) {
-    if ((lib->library)[i]) 
-      (lib->library)[i] = free_list((lib->library)[i]);
-    i++;
-  }
-}
-
-// delete a song
-struct Symbols *remove_song(struct Symbols *lib, struct song_node *song) {
-  int x = toupper((song->artist)[0]-65);
-  (lib->library)[x] = remove_node((lib->library)[x], song);
-  return lib;
-}
-
 // shuffle - print out a series of random chosen songs
-struct Symbols *shuffle(struct Symbols *lib) {
+void shuffle(struct Symbols *lib)
+{
   srand(time(NULL));
+  struct Symbols *lib2 = lib;
   int i;
-  for (i=0; i<27; i++) {
-    if ((lib->library)[i]!=NULL) {
-      while ((lib->library)[i]) {
+  for (i = 0; i < 27; i++)
+  {
+    if ((lib2->library)[i] != NULL)
+    {
+      while ((lib2->library)[i])
+      {
         if (rand() % 100 > 50)
-          printf("Artist: %s\tSong: %s\n", (lib->library)[i]->artist, (lib->library)[i]->name);
-        (lib->library)[i] = (lib->library)[i]->next; 
+          printf("Artist: %s\tSong: %s\n", (lib2->library)[i]->artist, (lib2->library)[i]->name);
+        (lib2->library)[i] = (lib2->library)[i]->next;
       }
     }
   }
 }
 
+// delete a song
+struct Symbols *remove_song(struct Symbols *lib, char *name, char *artist)
+{
+  //struct song_node *song = new_Song(name, artist);
+  int x = toupper((artist)[0] - 65);
+  (lib->library)[x] = remove_node((lib->library)[x], name, artist);
+  return lib;
+}
 
+// clear the library
+void clear_lib(struct Symbols *lib)
+{
+  int i;
+  for (i = 0; i < 27; i++)
+  {
+    if ((lib->library)[i])
+      (lib->library)[i] = free_list((lib->library)[i]);
+  }
+}
